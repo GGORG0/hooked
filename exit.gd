440f4d9570abac2b472d00f5c0c3135dceb464f8
GGORG <GGORG0@protonmail.com>
2025-08-06 15:37:14 +0200

    animate player

--- exit.gd
+++ exit.gd
@@ -2,5 +2,5 @@ extends Node
 
 
 func _process(_delta: float) -> void:
-    if Input.is_action_pressed("exit"):
-        get_tree().quit()
+	if Input.is_action_pressed("exit"):
+		get_tree().quit()
--- player.gd
+++ player.gd
@@ -6,16 +6,21 @@ const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
-    if not is_on_floor():
-        velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-    if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-    var direction := Input.get_axis("left", "right")
-    if direction:
-        velocity.x = direction * SPEED
-    else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
 
-    move_and_slide()
+		$AnimatedSprite2D.flip_h = direction < 0
+		$AnimatedSprite2D.play("walk")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		$AnimatedSprite2D.play("idle")
+
+	move_and_slide()
