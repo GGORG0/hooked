7ec5601e2cfc4f03a61b87ff360acff23e9607bd
GGORG <GGORG0@protonmail.com>
2025-08-08 21:29:46 +0200

    made the viewport scale to not show too much of the map on big screens

--- camera_2d.gd
+++ camera_2d.gd
@@ -4,13 +4,16 @@ extends Camera2D
 const DEAD_ZONE = 160
 
 
+@onready var viewport := get_viewport()
+
+
 var is_mouse_panning := false
 
 
 # Mouse panning
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		var viewport_size = get_viewport().size
+		var viewport_size = viewport.get_visible_rect().size
 
 		var mouse_pos = event.position.clamp(Vector2.ZERO, viewport_size)
 		var target = mouse_pos - viewport_size * 0.5
@@ -25,7 +28,7 @@ func _input(event: InputEvent) -> void:
 
 # Gamepad panning
 func _process(_delta: float) -> void:
-	var viewport_size = get_viewport().size
+	var viewport_size = viewport.get_visible_rect().size
 	var horizontal = Input.get_axis("pan_left", "pan_right")
 	var vertical = Input.get_axis("pan_up", "pan_down")
 
