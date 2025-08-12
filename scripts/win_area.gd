5b4e87a881775b99f8e9e23657668484864c0343
GGORG <GGORG0@protonmail.com>
2025-08-12 22:43:47 +0200

    added time summary in the win dialog

--- scripts/win_area.gd
+++ scripts/win_area.gd
@@ -3,6 +3,8 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		var win = ModalWindowManager.create('Stay tuned for more levels.', 'You won!')
+		var time = Time.get_ticks_msec() / 1000.0
+		var time_int = Time.get_time_string_from_unix_time(time)
+		var win = ModalWindowManager.create('It took you %s!\nStay tuned for more levels.' % time_int, 'You won!')
 		await win.wait_to_close()
 		get_tree().quit()
