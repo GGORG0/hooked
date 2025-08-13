232a08203a819a1a8a6f08785de1c16e2cb63243
GGORG <GGORG0@protonmail.com>
2025-08-13 20:07:23 +0200

    added milliseconds to win dialog timer

--- scripts/win_area.gd
+++ scripts/win_area.gd
@@ -3,8 +3,11 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		var time = Time.get_ticks_msec() / 1000.0
-		var time_int = Time.get_time_string_from_unix_time(time)
-		var win = ModalWindowManager.create('It took you %s!\nStay tuned for more levels.' % time_int, 'You won!')
+		var time = Time.get_ticks_msec()
+		var minutes = int(time / 60000)
+		var seconds = int((time % 60000) / 1000)
+		var milliseconds = int((time % 1000) / 10)
+		var time_str = '%02d:%02d.%02d' % [minutes, seconds, milliseconds]
+		var win = ModalWindowManager.create('It took you %s!\nStay tuned for more levels.' % time_str, 'You won!')
 		await win.wait_to_close()
 		get_tree().quit()
